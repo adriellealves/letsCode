1cf3ef8a895b2dd09952db27874048963b0438cd
Adrielle <67525959+adriellealves@users.noreply.github.com>
2023-03-21 13:24:06 -0300

    Update endpoint.js

--- src/service/endpoint.js
+++ src/service/endpoint.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : 'http://172.18.111.93:3000/'
+    baseURL : 'http://localhost:3000'
 })
 
 export default api;
